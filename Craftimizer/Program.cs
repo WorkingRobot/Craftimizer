bea568013c2500fd0bf14f3a09ec3c5e25ea7e03
Asriel Camora <asriel@camora.dev>
2023-06-12 10:21:21 -0700

    Initial commit

---
+++ Craftimizer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Craftimizer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Craftimizer/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Craftimizer;
+
+internal record Recipe
+{
+    public int Difficulty { get; init; }
+    public int Durability { get; init; }
+    public int Quality { get; init; }
+    public int Progress { get; init; }
+    public int Level { get; init; }
+    public int MaxLevel { get; init; }
+    public int ID { get; init; }
+}
