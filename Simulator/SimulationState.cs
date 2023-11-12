c8b963d05113221d91516e93604b23498fbb45d9
Asriel Camora <asriel@camora.dev>
2023-11-11 23:58:46 -0800

    Memory optimization stuff

--- Simulator/SimulationState.cs
+++ Simulator/SimulationState.cs
@@ -18,7 +18,7 @@ public record struct SimulationState
     public ActionStates ActionStates;
 
     // https://github.com/ffxiv-teamcraft/simulator/blob/0682dfa76043ff4ccb38832c184d046ceaff0733/src/model/tables.ts#L2
-    private static readonly int[] HQPercentTable = {
+    private static ReadOnlySpan<int> HQPercentTable => new[] {
         1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8,
         9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 15, 15, 15, 16, 16, 17, 17,
         17, 18, 18, 18, 19, 19, 20, 20, 21, 22, 23, 24, 26, 28, 31, 34, 38, 42, 47, 52, 58, 64, 68, 71,
@@ -44,13 +44,4 @@ public record struct SimulationState
         ActionCount = 0;
         ActionStates = new();
     }
-
-#if IS_DETERMINISTIC
-    public override readonly string ToString()
-    {
-        var b = new System.Text.StringBuilder();
-        PrintMembers(b);
-        return Convert.ToHexString(System.Security.Cryptography.SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(b.ToString())));
-    }
-#endif
 }
