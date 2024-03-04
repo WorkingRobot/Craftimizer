923f29cf3cea4fbfa6d08002da92f2f2209f1882
Asriel Camora <asriel@camora.dev>
2024-03-03 19:00:39 -0800

    Increase arena node size 24 -> 32

--- Solver/ArenaBuffer.cs
+++ Solver/ArenaBuffer.cs
@@ -4,31 +4,34 @@ using System.Runtime.CompilerServices;
 
 namespace Craftimizer.Solver;
 
-// Adapted from https://github.com/dtao/ConcurrentList/blob/4fcf1c76e93021a41af5abb2d61a63caeba2adad/ConcurrentList/ConcurrentList.cs
-public struct ArenaBuffer<T> where T : struct
+public struct ArenaBuffer
 {
     // Technically 25, but it's very unlikely to actually get to there.
     // The benchmark reaches 20 at most, but here we have a little leeway just in case.
-    private const int MaxSize = 24;
+    internal const int MaxSize = 32;
 
-    private static readonly int BatchSize = Vector<float>.Count;
-    private static readonly int BatchSizeBits = int.Log2(BatchSize);
-    private static readonly int BatchSizeMask = BatchSize - 1;
+    internal static readonly int BatchSize = Vector<float>.Count;
+    internal static readonly int BatchSizeBits = int.Log2(BatchSize);
+    internal static readonly int BatchSizeMask = BatchSize - 1;
 
-    private static readonly int BatchCount = MaxSize / BatchSize;
+    internal static readonly int BatchCount = MaxSize / BatchSize;
+}
 
+// Adapted from https://github.com/dtao/ConcurrentList/blob/4fcf1c76e93021a41af5abb2d61a63caeba2adad/ConcurrentList/ConcurrentList.cs
+public struct ArenaBuffer<T> where T : struct
+{
     public ArenaNode<T>[][] Data;
     public int Count { get; private set; }
 
     public void Add(ArenaNode<T> node)
     {
-        Data ??= new ArenaNode<T>[BatchCount][];
+        Data ??= new ArenaNode<T>[ArenaBuffer.BatchCount][];
 
         var idx = Count++;
 
         var (arrayIdx, subIdx) = GetArrayIndex(idx);
 
-        Data[arrayIdx] ??= new ArenaNode<T>[BatchSize];
+        Data[arrayIdx] ??= new ArenaNode<T>[ArenaBuffer.BatchSize];
 
         node.ChildIdx = (arrayIdx, subIdx);
         Data[arrayIdx][subIdx] = node;
@@ -37,5 +40,5 @@ public struct ArenaBuffer<T> where T : struct
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static (int arrayIdx, int subIdx) GetArrayIndex(int idx) =>
-        (idx >> BatchSizeBits, idx & BatchSizeMask);
+        (idx >> ArenaBuffer.BatchSizeBits, idx & ArenaBuffer.BatchSizeMask);
 }
