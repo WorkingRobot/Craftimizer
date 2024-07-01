736a9a7c459e6ac51df201e88f05b0a3938c60e3
Asriel Camora <asriel@camora.dev>
2024-07-01 16:32:35 -0700

    Fix combo

--- Simulator/Actions/ObservedAdvancedTouchCombo.cs
+++ Simulator/Actions/ObservedAdvancedTouchCombo.cs
@@ -1,7 +1,7 @@
 namespace Craftimizer.Simulator.Actions;
 
 internal sealed class ObservedAdvancedTouchCombo() : BaseComboAction<Observe, AdvancedTouch>(
-    ActionType.StandardTouchCombo, ActionType.AdvancedTouch, 7 + 18
+    ActionType.Observe, ActionType.AdvancedTouch, 7 + 18
     )
 {
 
